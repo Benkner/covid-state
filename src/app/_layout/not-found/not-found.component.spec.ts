8b51ee221574b0351e22237832826a260b215800
Benkner <75499975+Benkner@users.noreply.github.com>
2021-04-13 21:40:34 +0200

    fixed test

--- src/app/_layout/not-found/not-found.component.spec.ts
+++ src/app/_layout/not-found/not-found.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { NotFoundComponent } from './not-found.component';
+
 
 describe('NotFoundComponent', () => {
   let component: NotFoundComponent;
@@ -8,9 +9,10 @@ describe('NotFoundComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NotFoundComponent ]
+      imports: [RouterTestingModule],
+      declarations: [NotFoundComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
--- src/app/_layout/not-found/not-found.module.ts
+++ src/app/_layout/not-found/not-found.module.ts
@@ -1,8 +1,6 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
 import { NotFoundRoutingModule } from './not-found-routing.module';
-
 
 @NgModule({
   declarations: [],
