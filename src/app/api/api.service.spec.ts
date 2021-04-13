5ac6026019b42a5e41579ad61aa55cee104402a0
Benkner <75499975+Benkner@users.noreply.github.com>
2021-04-13 22:33:46 +0200

    api tests

--- src/app/api/api.service.spec.ts
+++ src/app/api/api.service.spec.ts
@@ -15,4 +15,33 @@ describe('ApiService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return 16 state elements', (done) => {
+    service.getStateList().subscribe(states => {
+      expect(states.length).toEqual(16);
+      expect(states[0].id).toEqual(jasmine.any(Number));
+      expect(states[0].name).toEqual(jasmine.any(String));
+      done();
+    });
+  });
+
+  it('should return valid statistics for germany', (done) => {
+    service.getHistoryGermany(2).subscribe(stats => {
+      expect(stats.cases).toEqual(jasmine.any(Number));
+      expect(stats.deaths).toEqual(jasmine.any(Number));
+      expect(stats.recovered).toEqual(jasmine.any(Number));
+      expect(stats.status).toEqual(jasmine.any(String));
+      done();
+    });
+  });
+
+  it('should return valid statistics for a state', (done) => {
+    service.getHistoryState(2, 1).subscribe(stats => {
+      expect(stats.cases).toEqual(jasmine.any(Number));
+      expect(stats.deaths).toEqual(jasmine.any(Number));
+      expect(stats.recovered).toEqual(jasmine.any(Number));
+      expect(stats.status).toEqual(jasmine.any(String));
+      done();
+    });
+  });
 });
